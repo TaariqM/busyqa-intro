154d1720235b79e81fc2aac1766c71a53fe2f889
TaariqM <taariqmcdonald@gmail.com>
2024-09-01 13:26:29 -0400

    Completed Intro to React Components Task

---
+++ Lab_7 - React and Single Page Applications/Task - Intro to Components/my-react-app/src/App.js
@@ -0,0 +1,15 @@
+// import logo from "./logo.svg";
+// import "./App.css";
+import React from "react";
+import Greeter from "./Greeter";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App</h1>
+      <Greeter />
+    </div>
+  );
+}
+
+export default App;
