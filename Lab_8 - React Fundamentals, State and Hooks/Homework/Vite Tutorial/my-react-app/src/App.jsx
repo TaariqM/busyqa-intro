53eab2d5111872248384f9c523d0324edae28b60
TaariqM <taariqmcdonald@gmail.com>
2024-09-07 22:28:17 -0400

    converted CRA app to vite in the Lab 7 tutorial and completed the Vite tutorial for Module 8

---
+++ Lab_8 - React Fundamentals, State and Hooks/Homework/Vite Tutorial/my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import Welcome from "./components/Welcome";
+// import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Welcome />
+    </>
+  );
+}
+
+export default App;
