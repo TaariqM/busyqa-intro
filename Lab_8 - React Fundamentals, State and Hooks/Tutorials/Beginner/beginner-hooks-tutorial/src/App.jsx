89737087eab6002068b73fd0584e0b728f2f9b7b
TaariqM <taariqmcdonald@gmail.com>
2024-09-10 00:32:14 -0400

    Finished react hooks beginner tutorial

--- Lab_8 - React Fundamentals, State and Hooks/Tutorials/Beginner/beginner-hooks-tutorial/src/App.jsx
+++ Lab_8 - React Fundamentals, State and Hooks/Tutorials/Beginner/beginner-hooks-tutorial/src/App.jsx
@@ -3,12 +3,9 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Counter from "./Counter";
 import DataFetching from "./DataFetching";
-
 import "./App.css";
 
 function App() {
-  const [reduceCount, setReduceCount] = useState(0);
-
   return (
     <>
       <Counter />
